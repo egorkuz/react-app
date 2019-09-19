9f8b51521ceb5623670d7a409d32cfe4465b5608
MagicMan <microblogworkingmail@mail.ru>
2019-09-20 01:05:21 +0300

    Updates

---
+++ precache-manifest.868ea30494e5d03ba56c3ed6e1408998.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b69d69a535491a4e938dc1da03ff86b6",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "48c528934535a614d14b",
+    "url": "/react-app/static/css/3.71137095.chunk.css"
+  },
+  {
+    "revision": "9ed0e6b39437e409a341",
+    "url": "/react-app/static/css/main.e7f6cbf2.chunk.css"
+  },
+  {
+    "revision": "3f8e017c9bed8cffcf12",
+    "url": "/react-app/static/js/2.5e90bf4b.chunk.js"
+  },
+  {
+    "revision": "48c528934535a614d14b",
+    "url": "/react-app/static/js/3.770b7f08.chunk.js"
+  },
+  {
+    "revision": "9ed0e6b39437e409a341",
+    "url": "/react-app/static/js/main.ca4feb06.chunk.js"
+  },
+  {
+    "revision": "8c58ac47c46e3a2cc3df",
+    "url": "/react-app/static/js/runtime~main.8f8f3501.js"
+  },
+  {
+    "revision": "cffa9d9d88890cfcd0ae3d6dd7a6efa9",
+    "url": "/react-app/static/media/load.cffa9d9d.gif"
+  }
+]);
