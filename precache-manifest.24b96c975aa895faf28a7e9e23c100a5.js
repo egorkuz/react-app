1721c78a96a77e1ce1ff2b84c5c930fe6fb325cc
MagicMan <microblogworkingmail@mail.ru>
2019-11-17 15:56:21 +0300

    Updates

---
+++ precache-manifest.24b96c975aa895faf28a7e9e23c100a5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1847fdd16633ded31e33e9125360bad",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "b8f117bf05d5b6f5a0b9",
+    "url": "/react-app/static/css/3.8b1becbe.chunk.css"
+  },
+  {
+    "revision": "492335815209463d42cd",
+    "url": "/react-app/static/css/main.1d46daad.chunk.css"
+  },
+  {
+    "revision": "29eab5840b0b2099750d",
+    "url": "/react-app/static/js/2.1d9a6e92.chunk.js"
+  },
+  {
+    "revision": "b8f117bf05d5b6f5a0b9",
+    "url": "/react-app/static/js/3.9ac6d04b.chunk.js"
+  },
+  {
+    "revision": "492335815209463d42cd",
+    "url": "/react-app/static/js/main.0953f8d9.chunk.js"
+  },
+  {
+    "revision": "ed5b8b3ee393b80008f7",
+    "url": "/react-app/static/js/runtime~main.a573326f.js"
+  },
+  {
+    "revision": "5a6a8108729ff26be0d519895e1c37f1",
+    "url": "/react-app/static/media/commentary.5a6a8108.png"
+  },
+  {
+    "revision": "3308ee5fbe5ef99d1cf6c3f00fca6c69",
+    "url": "/react-app/static/media/like.3308ee5f.png"
+  },
+  {
+    "revision": "cffa9d9d88890cfcd0ae3d6dd7a6efa9",
+    "url": "/react-app/static/media/load.cffa9d9d.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.327a005e30905b8e2fe489750452b882.js"
+  "/react-app/precache-manifest.24b96c975aa895faf28a7e9e23c100a5.js"
 );
 
 self.addEventListener('message', (event) => {
